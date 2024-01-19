7073c66a80d9593dfdc178480c8095d9ff08770b
Yasmin <yasminbanu1605@gmail.com>
2024-01-19 13:02:24 +0530

    TASK : Crud API

--- form/routes/api.php
+++ form/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ApiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Display Data 
+Route::get('display/{id?}',[ApiController::class,"display"] );
+
+//Insert Data
+Route::post("add",[ApiController::class,"addProduct"] );
+
+//Update Data
+Route::put("update",[ApiController::class,"updateProduct"]);
+
+//Search Data
+Route::get("search/{id}",[ApiController::class,"searchProduct"]);
+
+//Delete Data
+Route::delete("delete/{id}",[ApiController::class,"deleteProduct"]);
+
